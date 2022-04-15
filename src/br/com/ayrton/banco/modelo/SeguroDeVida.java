6b90675f9da4df019546f468a650d78bbf15473a
Ayrton <devayrtonsbarreto@gmail.com>
2022-04-15 10:44:37 -0300

    importando projeto do curso anterior (java.lang)

---
+++ src/br/com/ayrton/banco/modelo/SeguroDeVida.java
@@ -0,0 +1,10 @@
+package br.com.ayrton.banco.modelo;
+
+public class SeguroDeVida implements Tributavel {
+
+	@Override
+	public double getValorImposto() {
+		return 42;
+	}
+
+}
---
+++ src/br/com/ayrton/banco/modelo/Tributavel.java
@@ -0,0 +1,6 @@
+package br.com.ayrton.banco.modelo;
+
+public interface Tributavel {
+
+	double getValorImposto();
+}
